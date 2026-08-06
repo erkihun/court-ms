5be2f3417f1fac834ed09921ef9bed7cef28aef5
Erkihun <erkihunbewket@gmail.com>
2026-08-06 15:44:28 +0300

    qua

--- app/Notifications/ApplicantEmailOtp.php
+++ app/Notifications/ApplicantEmailOtp.php
@@ -2,11 +2,15 @@
 
 namespace App\Notifications;
 
-use Illuminate\Notifications\Notification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
 
-class ApplicantEmailOtp extends Notification
+class ApplicantEmailOtp extends Notification implements ShouldQueue
 {
+    use Queueable;
+
     public function __construct(protected string $code) {}
 
     public function via($notifiable): array
@@ -18,9 +22,9 @@ class ApplicantEmailOtp extends Notification
     {
         return (new MailMessage)
             ->subject('Your Email Verification Code')
-            ->greeting('Hello ' . ($notifiable->full_name ?? 'Applicant') . ',')
+            ->greeting('Hello '.($notifiable->full_name ?? 'Applicant').',')
             ->line('Use the 6-digit code below to verify your email address:')
-            ->line('# ' . $this->code)
+            ->line('# '.$this->code)
             ->line('This code expires in **10 minutes**.')
             ->line('If you did not create an account, no further action is required.');
     }
